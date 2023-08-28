94260bc342db042f56f6cc3361d3a509a2b866ad
Jacobo149 <jtp323@lehigh.edu>
2023-08-27 22:08:02 -0400

    Array Generator Created

---
+++ sources/array_generator.py
@@ -0,0 +1,13 @@
+## File to generate random array of 10000 elements ###
+
+import random
+
+def generate_array():
+    array = []
+    for i in range(10000):
+        array.append(random.randint(0, 10000))
+    return array
+
+
+if __name__ == "__main__":
+    print(generate_array())
