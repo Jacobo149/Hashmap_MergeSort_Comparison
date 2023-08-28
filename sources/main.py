307b451eaeb6c2868cb0b7e41c2608ad457ee43c
Jacobo149 <jtp323@lehigh.edu>
2023-08-28 09:49:21 -0400

    Small Additions to Main

--- sources/main.py
+++ sources/main.py
@@ -1,6 +1,18 @@
-# Program to compare speeds between merge sort and a prime number hashmap
-# Will test by sending two arrays to check if they are equal to one another
+''' Program to compare speeds between merge sort and a prime number hashmap
+Will test by sending two arrays to check if they are equal to one another '''
 
 from sources.mergesort import mergeSort
 from sources.verifier import verify
 from sources.hashmap import hashmap
+from sources.array_generator import generate_array
+
+def main():
+    # Generate array of 1000 random integers
+    arr = generate_array()
+
+    # Generate array of 1000 prime numbers
+    primes = primeNumberGenerator()
+
+
+    # Sort array of 1000 random integers
+    arr = mergeSort(arr)
